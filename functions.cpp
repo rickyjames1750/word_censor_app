31fd4d8185a07d55b2d343b27d7625c2045cf42f
rickyjames1750 <rickyjames1750@gmail.com>
2022-12-28 04:42:24 -0800

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,6 +1,6 @@
 #include <string>
 
-void asterik(std::string word, std::string &text, int i) {
+void asterisk(std::string word, std::string &text, int i) {
 
     for (int k = 0; k < word.size(); ++k) {
         text[i+k] = '*';
@@ -8,20 +8,27 @@ void asterik(std::string word, std::string &text, int i) {
 }
 
 void bleep(std::string word, std::string &text) {
-
-    for (int i = 0; j < word.size(); ++j) {
-
-        int match = 0;
-
-        for (int j = 0; j < word.size(); ++j) {
-            if (text[i+j] == word[j]) {
-                ++match;
-                }
-            }
-            
-            if (match == word.size()) {
-                
-                asterik(word, text, i);
-        }
+    
+  for (int i = 0; i < text.size(); ++i) {
+        
+    int match = 0;
+        
+    for (int j = 0; j < word.size(); ++j) {
+    
+      if (text[i+j] == word[j]) {
+        
+        ++match;
+          
+      }
+    
     }
+        
+    if (match == word.size()) {
+            
+      asterisk(word, text, i);
+        
+    }
+      
+  }
+    
 }
